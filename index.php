8e9f831ff052bb68e46a297565afc60f3ac911a7
DutchOffensive <52233154+DutchOffensive@users.noreply.github.com>
2020-02-04 12:30:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">   
         <meta name="description" content="">
         <meta name="keywords" content="">
-        <meta name="author" content="">
+        <meta name="author" content="Adam, Youri, Simon">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
         <link href="style/main.css" rel="stylesheet" type="text/css">
