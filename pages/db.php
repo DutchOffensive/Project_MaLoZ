8dd5d1e3bd2673471aeb56d2be20dda5db0ff4c4
DutchOffensive <52233154+DutchOffensive@users.noreply.github.com>
2020-02-18 12:43:16 +0100

    Update db.php

--- pages/db.php
+++ pages/db.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'db.simonschiks.nl');
-define('DB_USERNAME', 'md468796db499733');
-define('DB_PASSWORD', 'Test123!@#');
-define('DB_NAME', 'md468796db499733');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'maloz');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
