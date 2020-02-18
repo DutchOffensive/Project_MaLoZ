363d6de57c5775e7a6dd358f452867ff07af9e66
DutchOffensive <52233154+DutchOffensive@users.noreply.github.com>
2020-02-18 12:56:53 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="nl">
-
-<head>
-
-	<link href="style/main.css" rel="stylesheet" type="text/css">
-	<!-- bootstrap -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
-	<title>Project MaLoZ | Register</title>
-
-</head>
-
-<body>
 	<div class="content">
 	<form name="registeren" class="form" method="POST" >
         <h1 id="pagina_titel">Register</h1>
@@ -36,7 +19,7 @@
 
 				<input class="btn btn-outline-success my-2 my-sm-0" type="submit" class="icon" id="submit" name="submit" value="Submit" />
 			</div>
-			<a class="btn btn-outline-danger my-2 my-sm-0" href="index.php">Back</a>
+			<a class="btn btn-outline-danger my-2 my-sm-0" href="/index.php">Back</a>
 		</form>
 	</div>
 <?php 
@@ -89,6 +72,3 @@ if(isset($_POST["submit"])){
 	}
 
 	?>
-</body>
-
-</html>
